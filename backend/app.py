379cb632f18f1bde73bca4778facf9ea26e107d3
AkkusEmre34 <akkusemre0321@gmail.com>
2026-07-21 13:30:40 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,28 @@
+from flask import Flask, render_template, request
 from ai_engine import find_answer
 
-print("Smart Support AI")
-print("Çıkmak için 'çıkış' yazın.")
+app = Flask(
+    __name__,
+    template_folder="../frontend/templates",
+    static_folder="../frontend/static"
+)
 
-while True:
-    question = input("\nSorununuzu yazın: ")
 
-    if question.lower() == "çıkış":
-        print("Program kapatıldı.")
-        break
+@app.route("/", methods=["GET", "POST"])
+def home():
+    answer = None
+    question = ""
 
-    answer = find_answer(question)
-    print("Cevap:", answer)
+    if request.method == "POST":
+        question = request.form.get("question", "")
+        answer = find_answer(question)
+
+    return render_template(
+        "index.html",
+        answer=answer,
+        question=question
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
